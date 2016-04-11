f3c7dcd98d60871fd1defccda4b7bbe82391c9b2
Wang Yiwen <Yiwen.Wang@alcatel-lucent.com>
2016-04-12 00:05:53 +0800

    second Change two files print content

--- hello.sh
+++ hello.sh
@@ -3,3 +3,4 @@
 #email: wywbuct@126.com
 echo "my name is wangyiwen"
 echo "zhen shi yumen si le"
+echo "modify second time,date is 2016.04.11"
--- wyw.sh
+++ wyw.sh
@@ -2,4 +2,4 @@
 #author: wangyiwen
 #email: wywbuct@126.com
 echo "my name is wangyiwen"
-
+echo "modify second time date 2016.4.11"
