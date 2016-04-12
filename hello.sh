cf5d34482b158329a3f7d6316e63fa3aff95f888
Wang Yiwen <Yiwen.Wang@alcatel-lucent.com>
2016-04-12 23:42:56 +0800

    add static by error switch branch master

--- hello.sh
+++ hello.sh
@@ -5,3 +5,5 @@ echo "my name is wangyiwen"
 echo "zhen shi yumen si le"
 echo "modify second time,date is 2016.04.11"
 echo "modify by wnagyiwen $date"
+
+echo "hello master by error switch branch master"
